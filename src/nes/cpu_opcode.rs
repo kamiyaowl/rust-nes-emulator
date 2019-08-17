6d8196a6442ec2f144375309a24a4a09e7a48caa
K <kamiyaowl@gmail.com>
2019-08-17 12:00:58 +0900

    macro完全に理解した(何もわからない)

--- src/nes/cpu_opcode.rs
+++ src/nes/cpu_opcode.rs
@@ -8,14 +8,14 @@ macro_rules! inst {
     (
         $self:expr, $system:expr,
         $name:expr, cycle => $cycle:expr, pc_incr => $pc_incr:expr,
-        $adressing_closure:expr,
+        $addressing_closure:expr,
         $inst_closure:expr
     ) => {
         {
             if cfg!(debug_assertions) {
                 println!("[before][#{}] cycle:{} pc_incr:{} pc{:04x} a:{:02x} x:{:02x} y:{:02x} sp:{:04x} p:{:08b}", $name, $cycle, $pc_incr, $self.pc, $self.a, $self.x, $self.y, $self.sp, $self.p);
             }
-            let addr = $adressing_closure();
+            let addr = $addressing_closure();
             let data = $system.read_u8(addr);
             $self.increment_pc($pc_incr);
             $inst_closure(addr, data);
@@ -26,17 +26,28 @@ macro_rules! inst {
         }
     };
     (
-        $self:expr, $system:expr,
-        [
+        $self:expr, $system:expr, $target_opcode:expr,
+        $(
+            {
+                $name:expr, opcode => $opcode:expr, cycle => $cycle:expr, pc_incr => $pc_incr:expr,
+                $addressing_closure:expr,
+                $inst_closure:expr
+            }
+        ),*
+    ) => {
+        match $target_opcode {
             $(
-                $opcode => {
-                    $name:expr, cycle => $cycle:expr, pc_incr => $pc_incr:expr,
-                    $adressing_closure:expr,
-                    $inst_closure:expr
-                }
-            ),*
-        ]
-    ) => ()
+                $opcode => inst!($self, $system,
+                    $name, cycle => $cycle, pc_incr => $pc_incr,
+                    $addressing_closure,
+                    $inst_closure
+                ),
+            )*
+            _ => {
+                panic!("invalid Operation. opcode:{:02x} pc:{:04x} a:{:02x} x:{:02x} y:{:02x} sp:{:04x} p:{:08b}", $target_opcode, $self.pc, $self.a, $self.x, $self.y, $self.sp, $self.p);
+            },
+        }
+    }
 
 }
 
@@ -48,136 +59,22 @@ impl Cpu {
     /// return: かかったclock cycle count`
     pub fn step(&mut self, system: &mut System) -> u8 {
         let opcode = system.read_u8(self.pc);
+        self.increment_pc(1);
         if cfg!(debug_assertions) {
             println!("[opcode fetched] opcode:{:02x} pc:{:04x} a:{:02x} x:{:02x} y:{:02x} sp:{:04x} p:{:08b}", opcode, self.pc, self.a, self.x, self.y, self.sp, self.p);
         }
-
-        self.increment_pc(1);
-
-        let cycles = match opcode {
-            /**************** ADC ****************/
-            0x69 => inst!(self, system,
-                "ADC imm", cycle => 2, pc_incr => 1, 
+        let cycles = inst!(self, system, opcode,
+            {
+                "Dummy", opcode => 0x00, cycle => 1, pc_incr => 1, 
+                || self.addressing_immediate(system, self.pc),
+                |addr, data| println!("Hello macro! addr:{:04x} data:{:02x}", addr, data)
+            },
+            {
+                "ADC imm", opcode => 0x69, cycle => 2, pc_incr => 1, 
                 || self.addressing_immediate(system, self.pc),
                 |_addr, data| self.inst_adc(data)
-            ),
-            // 0x69 => {
-            //     let addr = self.addressing_immediate(system, self.pc);
-            //     let data = system.read_u8(addr);
-            //     self.increment_pc(1);
-            //     self.inst_adc(data);
-            //     2
-            // },
-            0x65 => {
-                let addr = self.addressing_zero_page(system, self.pc);
-                let data = system.read_u8(addr);
-                self.increment_pc(1);
-                self.inst_adc(data);
-                3
-            },
-            0x75 => {
-                let addr = self.addressing_zero_page_indexed_x(system, self.pc);
-                let data = system.read_u8(addr);
-                self.increment_pc(1);
-                self.inst_adc(data);
-                4
-            },
-            0x6d => {
-                let addr = self.addressing_absolute(system, self.pc);
-                let data = system.read_u8(addr);
-                self.increment_pc(2);
-                self.inst_adc(data);
-                4
-            },
-            0x7d => {
-                let addr = self.addressing_absolute_indexed_x(system, self.pc);
-                let data = system.read_u8(addr);
-                self.increment_pc(2);
-                self.inst_adc(data);
-                4
-            },
-            0x79 => {
-                let addr = self.addressing_absolute_indexed_y(system, self.pc);
-                let data = system.read_u8(addr);
-                self.increment_pc(2);
-                self.inst_adc(data);
-                4
-            },
-            0x61 => {
-                let addr = self.addressing_indexed_indirect(system, self.pc);
-                let data = system.read_u8(addr);
-                self.increment_pc(1);
-                self.inst_adc(data);
-                6
-            },
-            0x71 => {
-                let addr = self.addressing_indirect_indexed(system, self.pc);
-                let data = system.read_u8(addr);
-                self.increment_pc(1);
-                self.inst_adc(data);
-                5
-            },
-            /**************** AND ****************/
-            /**************** ASL ****************/
-            /**************** BCC ****************/
-            /**************** BCS ****************/
-            /**************** BEQ ****************/
-            /**************** BIT ****************/
-            /**************** BMI ****************/
-            /**************** BNE ****************/
-            /**************** BPL ****************/
-            /**************** BRK ****************/
-            /**************** BVC ****************/
-            /**************** BVS ****************/
-            /**************** CLC ****************/
-            /**************** CLD ****************/
-            /**************** CLI ****************/
-            /**************** CLV ****************/
-            /**************** CMP ****************/
-            /**************** CPX ****************/
-            /**************** CPY ****************/
-            /**************** DEC ****************/
-            /**************** DEX ****************/
-            /**************** DEY ****************/
-            /**************** EOR ****************/
-            /**************** INC ****************/
-            /**************** INX ****************/
-            /**************** INY ****************/
-            /**************** JMP ****************/
-            /**************** JSR ****************/
-            /**************** LDA ****************/
-            /**************** LDX ****************/
-            /**************** LDY ****************/
-            /**************** LSR ****************/
-            /**************** NOP ****************/
-            /**************** ORA ****************/
-            /**************** PHA ****************/
-            /**************** PHP ****************/
-            /**************** PLA ****************/
-            /**************** PLP ****************/
-            /**************** ROL ****************/
-            /**************** ROR ****************/
-            /**************** RTI ****************/
-            /**************** RTS ****************/
-            /**************** SBC ****************/
-            /**************** SEC ****************/
-            /**************** SED ****************/
-            /**************** SEI ****************/
-            /**************** STA ****************/
-            /**************** STX ****************/
-            /**************** STY ****************/
-            /**************** TAX ****************/
-            /**************** TAY ****************/
-            /**************** TSX ****************/
-            /**************** TXA ****************/
-            /**************** TXS ****************/
-            /**************** TYA ****************/
-
-            /*********** unimplemented ***********/
-            _ => {
-                panic!("invalid Operation. opcode:{:02x} pc:{:04x} a:{:02x} x:{:02x} y:{:02x} sp:{:04x} p:{:08b}", opcode, self.pc, self.a, self.x, self.y, self.sp, self.p);
-            },
-        };
+            }
+        );
         cycles
     }
 
