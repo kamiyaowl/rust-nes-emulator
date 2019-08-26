403add69feb47c65bfd4d3ace820a36cf8225f30
K <kamiyaowl@gmail.com>
2019-08-26 23:15:41 +0900

    Ref #11 : Pad自体の実装は完了

---
+++ src/nes/pad.rs
@@ -0,0 +1,57 @@
+
+
+#[derive(Copy, Clone)]
+pub enum Button {
+    A,B,Select,Start,Up,Down,Left,Right
+}
+
+pub struct Pad {
+    /// 現在のボタン入力を示すシフトレジスタ
+    pub button_shift_reg: u8,
+    /// trueだと入力をロードし続ける
+    pub strobe_enable: bool,
+}
+impl Default for Pad {
+    fn default() -> Self {
+        Self {
+            button_shift_reg: 0,
+            strobe_enable: false,
+        }
+    }
+}
+
+impl Pad {
+    /// pad入力をクリアして取り込み直します
+    /// 0x4016/0x4017に1/0を
+    pub fn write_strobe(&mut self, is_enable: bool) {
+        self.strobe_enable = is_enable;
+    }
+    /// pad出力を読み取ります、シフトレジスタになっているため読むたびに状態を破壊します    
+    /// xxxx_xMES
+    /// M - microphone unimplement
+    /// E - Expansion controller unimplement
+    /// S - Primary controller
+    pub fn read_dout(&mut self) -> u8 {
+        let data = self.button_shift_reg & 0x01u8;
+        // strobeが無効であればデータを進める
+        if !self.strobe_enable {
+            self.button_shift_reg = self.button_shift_reg.wrapping_shr(1);
+        }
+        data
+    }
+    /// 押されたボタンをシフトレジスタに記録します。この関数はエミュの外側から呼ぶことを想定しています
+    pub fn push_button(&mut self, button: Button) {
+        if self.strobe_enable {
+            match button {
+                Button::A      => self.button_shift_reg = self.button_shift_reg & 0x01u8,
+                Button::B      => self.button_shift_reg = self.button_shift_reg & 0x02u8,
+                Button::Select => self.button_shift_reg = self.button_shift_reg & 0x04u8,
+                Button::Start  => self.button_shift_reg = self.button_shift_reg & 0x08u8,
+                Button::Up     => self.button_shift_reg = self.button_shift_reg & 0x10u8,
+                Button::Down   => self.button_shift_reg = self.button_shift_reg & 0x20u8,
+                Button::Left   => self.button_shift_reg = self.button_shift_reg & 0x40u8,
+                Button::Right  => self.button_shift_reg = self.button_shift_reg & 0x80u8,
+            }
+        }
+    }
+}
