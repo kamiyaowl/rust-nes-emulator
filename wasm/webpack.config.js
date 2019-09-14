8dcd331fe708fb0c9d80dc36293f250d830b1060
K <kamiyaowl@gmail.com>
2019-09-15 02:47:03 +0900

    Close #47 wasm動いたのでci修正

---
+++ wasm/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const src = __dirname;
+const dist = path.resolve(__dirname, "/dist");
+
+module.exports = {
+  context: src,
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js"
+  },
+  devServer: {
+    contentBase: dist,
+    port: 4444
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ]  
+};
