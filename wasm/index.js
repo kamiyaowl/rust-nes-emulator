d11969ea7a591901af3c085b6c599d02ee36b45d
K <kamiyaowl@gmail.com>
2019-09-15 18:23:31 +0900

    Close #60 wasm/src/lib.rsでjavascriptからエミュレータを実行できるようになった

--- wasm/index.js
+++ wasm/index.js
@@ -1,4 +1,16 @@
-const wasm = import('./node_modules/rust-nes-emulator-wasm/rust_nes_emulator_wasm.js');
-wasm.then(wasm => {
-  wasm.greet('WebAssembly');
-});
+async function main() {
+    const { memory }       = await import('./node_modules/rust-nes-emulator-wasm/rust_nes_emulator_wasm_bg');
+    const { WasmEmulator, get_screen_width, get_screen_height, get_num_of_colors } = await import('./node_modules/rust-nes-emulator-wasm/rust_nes_emulator_wasm.js');
+    const SCREEN_WIDTH  = get_screen_width();
+    const SCREEN_HEIGHT = get_screen_height();
+    const NUM_OF_COLORS = get_num_of_colors();
+
+    const emu = new WasmEmulator();
+    emu.reset();
+    const buf = new Uint8Array(memory.buffer);
+    const v = buf[emu.get_fb_ptr()];
+    console.log(v);
+
+}
+
+main();
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,15 +5,17 @@ extern crate rust_nes_emulator;
 use rust_nes_emulator::*;
 use rust_nes_emulator::interface::*;
 
-
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+pub fn get_screen_width() -> usize {
+    VISIBLE_SCREEN_WIDTH
 }
-
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn get_screen_height() -> usize {
+    VISIBLE_SCREEN_HEIGHT
+}
+#[wasm_bindgen]
+pub fn get_num_of_colors() -> usize {
+    NUM_OF_COLOR
 }
 
 #[wasm_bindgen]
@@ -24,12 +26,31 @@ pub struct WasmEmulator {
     ppu: Ppu,
     video_sys: VideoSystem,
 }
+impl Default for WasmEmulator {
+    fn default() -> Self {
+        Self {
+            fb: [[[0; NUM_OF_COLOR]; VISIBLE_SCREEN_WIDTH]; VISIBLE_SCREEN_HEIGHT],
+            cpu: Cpu::default(),
+            cpu_sys: System::default(),
+            ppu: Ppu::default(),
+            video_sys: VideoSystem::default(),
+        }
+    }
+}
 
 #[wasm_bindgen]
 impl WasmEmulator {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> WasmEmulator {
+        WasmEmulator::default()
+    }
     /// fbのポインタを取得します
     pub fn get_fb_ptr(&self) -> *const [[u8; NUM_OF_COLOR]; VISIBLE_SCREEN_WIDTH] {
         self.fb.as_ptr()
+    }
+    /// 1get_fb_ptr`で得られる配列のサイズを返します
+    pub fn get_fb_size(&self) -> usize {
+        NUM_OF_COLOR * VISIBLE_SCREEN_WIDTH * VISIBLE_SCREEN_HEIGHT
     }
     /// エミュレータをリセットします
     /// カセットの中身はリセットしないので実機のリセット相当の処理です
@@ -47,7 +68,15 @@ impl WasmEmulator {
         self.reset();
         self.cpu_sys.cassette.from_ines_binary(|addr: usize| binary[addr])
     }
-    /// 描画領域1line分を更新します
+    /// 描画領域1面分更新します
     pub fn step_line(&mut self) {
+        let cycle_for_draw_once = CPU_CYCLE_PER_LINE * usize::from(RENDER_SCREEN_HEIGHT + 1);
+        let mut total_cycle: usize = 0;
+        while total_cycle < cycle_for_draw_once {
+            let cpu_cycle = usize::from(self.cpu.step(&mut self.cpu_sys));
+            self.ppu.step(cpu_cycle, &mut self.cpu, &mut self.cpu_sys, &mut self.video_sys, &mut self.fb);
+            total_cycle = total_cycle + cpu_cycle;
+            // TODO: apu対応(1面分更新だとタイミング的に厳しいかも #8)
+        }
     }
 }
