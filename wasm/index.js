bf842b341b1b7c866dffe6ffbd0941dab0e4cbdf
K <kamiyaowl@gmail.com>
2019-09-16 03:38:18 +0900

    Close #59, Ref #61 fb内容をcanvasに表示する関数を作成

--- wasm/index.js
+++ wasm/index.js
@@ -12,13 +12,32 @@ async function main() {
   );
   const SCREEN_WIDTH = get_screen_width();
   const SCREEN_HEIGHT = get_screen_height();
-  const NUM_OF_COLORS = get_num_of_colors();
-
+  const NUM_OF_COLORS = get_num_of_colors(); // rust上での扱い、imageDataはalphaもある
   const emu = new WasmEmulator();
+  const rustBuf = new Uint8Array(memory.buffer);
+  const fbBasePtr = emu.get_fb_ptr();
+
+  function draw() {
+    const canvas = document.getElementById("fb");
+    const ctx = canvas.getContext("2d");
+    const imageData = ctx.getImageData(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    for (let j = 0; j < SCREEN_HEIGHT; j++) {
+      for (let i = 0; i < SCREEN_WIDTH; i++) {
+        const imageDataPtr = j * (SCREEN_WIDTH * 4) + i * 4;
+        const rustDataPtr =
+          fbBasePtr + j * (SCREEN_WIDTH * NUM_OF_COLORS) + i * NUM_OF_COLORS;
+        imageData.data[imageDataPtr + 0] = rustBuf[rustDataPtr + 0]; // red
+        imageData.data[imageDataPtr + 1] = rustBuf[rustDataPtr + 1]; // green
+        imageData.data[imageDataPtr + 2] = rustBuf[rustDataPtr + 2]; // blue
+        imageData.data[imageDataPtr + 3] = 255; //alpha
+      }
+    }
+    ctx.putImageData(imageData, 0, 0);
+  }
+  
   emu.reset();
-  const buf = new Uint8Array(memory.buffer);
-  const v = buf[emu.get_fb_ptr()];
-  console.log(v);
+  draw();
+  let isEmulateEnable = false;
 
   ELEMENT.locale("ja", ELEMENT.lang.ja);
   const app = new Vue({
@@ -45,6 +64,8 @@ async function main() {
         reader.onload = file => {
           const arrayBuf = file.target.result;
           const src = new Uint8Array(arrayBuf);
+          // stop emulate
+          isEmulateEnable = false;
           // cassette load
           if (!emu.load(src)) {
             // error notify
@@ -62,14 +83,20 @@ async function main() {
           });
         };
         reader.readAsArrayBuffer(e.target.files[0]);
+        // start emulate
+        emu.reset();
+        isEmulateEnable = true;
       },
       reset() {
+        isEmulateEnable = false;
         emu.reset();
         // notify
         const h = this.$createElement;
         this.$notify({
           title: "Emulator Reset"
         });
+        // start
+        isEmulateEnable = true;
       }
     }
   });
